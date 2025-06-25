0e9088501a8f31ef3b6073278ee46bbfa1ec8e8b
mduaali <143054848+mduaali@users.noreply.github.com>
2025-06-24 21:24:42 -0400

    Update extensions.py

--- fitfactor/extensions.py
+++ fitfactor/extensions.py
@@ -1,1 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# initializing SQLAlchemy
+db = SQLAlchemy()
+
+# initialize Login Manager?
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login' 
 # Use this for any third party libraries like sqlalchemy
