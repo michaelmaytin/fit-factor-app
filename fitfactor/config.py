9cf3b662e704f3247587e4c8c20b4a295e2cc381
mduaali <143054848+mduaali@users.noreply.github.com>
2025-06-24 21:32:04 -0400

    Update config.py

--- fitfactor/config.py
+++ fitfactor/config.py
@@ -1,2 +1,7 @@
 #settings for flask app, environment variables go here
 # create your own .env file in order to test environment variables
+
+class Config:
+    secret_key = 'supsecretkey'  
+    sql_alchemy_database_uri = 'sqlite:///fitfactor.db'
+    sql_alchemy_track_modifications = False
