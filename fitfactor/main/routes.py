5e0f465f106afddf25b062a7eb88bf27333c5ba9
brooksberry10 <brooksberry10@gmail.com>
2025-07-14 00:21:48 -0400

    minor set up update

--- fitfactor/main/routes.py
+++ fitfactor/main/routes.py
@@ -1,6 +1,6 @@
 # fitfactor/main/routes.py
 
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort, make_response
 from fitfactor.extensions import db
 from fitfactor.models import Role, Workout
 
